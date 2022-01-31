9bcfdcf111443fd74d42be773673cb41bac05e4d
abilnf <slin.lbeog@gmail.com>
2022-01-31 22:42:17 +0100

    Add form validation

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 document.querySelectorAll("input").forEach((input) => {
-  input.addEventListener("input", (e) => {
+  function validate(e) {
+    e.target.setCustomValidity("");
+    if (
+      e.target.validity.valid &&
+      e.target.getAttribute("id") === "confirm_password"
+    ) {
+      if (e.target.value !== document.querySelector("#password").value) {
+        e.target.setCustomValidity("Passwords do not match!");
+      }
+    }
+
+    e.target.parentElement.querySelector(".error").textContent =
+      input.validationMessage;
+
     e.preventDefault();
-  });
+  }
+  input.addEventListener("invalid", validate);
+  input.addEventListener("input", validate);
 });
